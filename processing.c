21bdd9fcc5cf24be5b8c3ddfa90eb0dc5422037c
Doohyeon Won (Daniel Harold Won) (Cor) <logicseeker@naver.com>
2024-02-24 01:33:26 +0900

    refactor

--- processing.c
+++ processing.c
@@ -170,95 +170,56 @@ inline static double window(int pos, int len) {
       return 1;
 }
 
-uint32_t process_wav(FILE *src,
-                     FILE *dest,
-                     struct wav_info *info,
-                     struct execution_options *options,
-                     bool is_le) {
+static uint32_t _shift_pitch(FILE *src,
+                             FILE *dest,
+                             struct wav_info *info,
+                             struct execution_options *options,
+                             bool is_le) {
+   int grain_size = options->size;
+   double pitch_factor = options->factor;
+   uint16_t num_channels = info->num_channels;
+   uint32_t total_sample = info->subchunk_2_size
+                           / (num_channels * (info->bits_per_sample / 8));
+   uint32_t total_unit = total_sample / grain_size;
+   int src_buf_len = grain_size * num_channels;
+   int dest_buf_len = src_buf_len;
+
    int result;
-   int grain_size;
+   int unit, channel;
+   int i;
+   double j;
    int16_t *src_buf, *dest_buf;
-   int buf_len;
+   uint32_t sample_number;
    
    result = fseek(dest, 44L, SEEK_SET);
    if (result != 0) raise_err("Failed to seek the file position.");
-
-   grain_size = options->size;
-   buf_len = grain_size * info->num_channels;
-   src_buf = malloc(buf_len * 2);
+   src_buf = malloc(src_buf_len * 2);
    if (src_buf == NULL) raise_err("Failed to allocate memory dynamically.");
+   dest_buf = malloc(dest_buf_len * 2);
+   if (dest_buf == NULL) raise_err("Failed to allocate memory dynamically.");
 
-   /* From here, data processing begins. */
-   int i, k, channel;
-   bool be = !is_le;
-   uint16_t num_channels = info->num_channels;
-   uint32_t total_sample = info->subchunk_2_size
-                           / (info->num_channels * (info->bits_per_sample / 8));
-   uint32_t total_unit = total_sample / grain_size;
-   uint32_t sample_number;
+   for (unit = 0; unit < total_unit; unit++) {
+      result = fread(src_buf, 2, src_buf_len, src);
+      if (result != src_buf_len) break;
 
-   /* pitch shifter */
-   if (options->mode == 1) {
-      double j;
-      double pitch_factor = options->factor;
-      
-      dest_buf = malloc(buf_len * 2);
-      if (dest_buf == NULL) raise_err("Failed to allocate memory dynamically.");
-
-      for (k = 0; k < total_unit; k++) {
-         result = fread(src_buf, 2, buf_len, src);
-         if (result != buf_len) break;
-
-         for (channel = 0; channel < num_channels; channel++)
-            for (i = 0, j = 0; i < grain_size; i++, j += pitch_factor) {
-               if (j >= grain_size)
-                  j = 0;
-               dest_buf[num_channels * i + channel]
-                  = src_buf[num_channels * (int) j + channel]
-                    * window(i, grain_size);
-            }
-         if (!is_le)
-            for (i = 0; i < buf_len; i++)
-               endrev16(&dest_buf[i]);
-
-         result = fwrite(dest_buf, 2, buf_len, dest);
-         if (result != buf_len) raise_err("Failed to write data.");
-      }
-      sample_number = k * grain_size;
-   }
-   /* time stretcher */
-   else if (options->mode == 2) {
-      int j;
-      int part;
-      int dest_buf_len;
-
-      part = grain_size / options->factor;
-      dest_buf_len = part * info->num_channels;
-      dest_buf = malloc(dest_buf_len * 2);
-      if (dest_buf == NULL) raise_err("Failed to allocate memory dynamically.");
-
-      for (k = 0; k < total_unit; k++) {
-         result = fread(src_buf, 2, buf_len, src);
-         if (result !=  buf_len) break;
-
-         for (channel = 0; channel < num_channels; channel++)
-            for (i = 0, j = 0; i < part; i++, j++) {
-               if (j == grain_size)
-                  j = 0;
-               dest_buf[num_channels * i + channel]
-                  = src_buf[num_channels * j + channel]
-                    * window(i, part);
-            }
-         if (!is_le)
+      for (channel = 0; channel < num_channels; channel++)
+         for (i = 0, j = 0; i < grain_size; i++, j += pitch_factor) {
+            if (j >= grain_size)
+               j = 0;
+            dest_buf[num_channels * i + channel]
+               = src_buf[num_channels * (int) j + channel]
+               * window(i, grain_size);
+         }
+      if (!is_le)
             for (i = 0; i < dest_buf_len; i++)
                endrev16(&dest_buf[i]);
 
-         result = fwrite(dest_buf, 2, dest_buf_len, dest);
-         if (result != dest_buf_len) raise_err("Failed to write data.");
-      }
-      sample_number = k * part;
+      result = fwrite(dest_buf, 2, dest_buf_len, dest);
+      if (result != dest_buf_len) raise_err("Failed to write data.");
    }
    if (ferror(src)) raise_err("Failed to read audio data.");
+
+   sample_number = unit * grain_size;
    if (sample_number % 2 == 1) {
       uint32_t padding = 0;
 
@@ -269,6 +230,87 @@ uint32_t process_wav(FILE *src,
    
    free(src_buf);
    free(dest_buf);
+   
+   return sample_number;
+}
+
+static uint32_t _stretch_time(FILE *src,
+                              FILE *dest,
+                              struct wav_info *info,
+                              struct execution_options *options,
+                              bool is_le) {
+   int grain_size = options->size;
+   double speed_factor = options->factor;
+   int part = grain_size / speed_factor;
+   uint16_t num_channels = info->num_channels;
+   uint32_t total_sample = info->subchunk_2_size
+                           / (num_channels * (info->bits_per_sample / 8));
+   uint32_t total_unit = total_sample / grain_size;
+   int src_buf_len = grain_size * num_channels;
+   int dest_buf_len = part * num_channels;
+
+   int result;
+   int unit, channel;
+   int i, j;
+   int16_t *src_buf, *dest_buf;
+   uint32_t sample_number;
+
+   result = fseek(dest, 44L, SEEK_SET);
+   if (result != 0) raise_err("Failed to seek the file position.");
+   src_buf = malloc(src_buf_len * 2);
+   if (src_buf == NULL) raise_err("Failed to allocate memory dynamically.");
+   dest_buf = malloc(dest_buf_len * 2);
+   if (dest_buf == NULL) raise_err("Failed to allocate memory dynamically.");
+
+   for (unit = 0; unit < total_unit; unit++) {
+      result = fread(src_buf, 2, src_buf_len, src);
+      if (result !=  src_buf_len) break;
+
+      for (channel = 0; channel < num_channels; channel++)
+         for (i = 0, j = 0; i < part; i++, j++) {
+            if (j == grain_size)
+               j = 0;
+            dest_buf[num_channels * i + channel]
+               = src_buf[num_channels * j + channel]
+                  * window(i, part);
+         }
+      if (!is_le)
+         for (i = 0; i < dest_buf_len; i++)
+            endrev16(&dest_buf[i]);
+
+      result = fwrite(dest_buf, 2, dest_buf_len, dest);
+      if (result != dest_buf_len) raise_err("Failed to write data.");
+   }
+   if (ferror(src)) raise_err("Failed to read audio data.");
+   
+   sample_number = unit * part;
+   if (sample_number % 2 == 1) {
+      uint32_t padding = 0;
+
+      result = fwrite(&padding, 4, 1, dest);
+      if (result != 1) raise_err("Failed to write data.");
+      sample_number++;
+   }
+   
+   free(src_buf);
+   free(dest_buf);
+
+   return sample_number;
+}
+
+uint32_t process_wav(FILE *src,
+                     FILE *dest,
+                     struct wav_info *info,
+                     struct execution_options *options,
+                     bool is_le) {
+   uint32_t sample_number;
+
+   if (options->mode == 1)
+      sample_number
+         = _shift_pitch(src, dest, info, options, is_le);
+   else if (options->mode == 2)
+      sample_number
+         = _stretch_time(src, dest, info, options, is_le);
 
    return sample_number;
 }
